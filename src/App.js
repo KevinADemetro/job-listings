f96caec002e27063908e64c4a0230a4833066cef
Kevin Demetro <albinodemetrok@gmail.com>
2025-05-10 01:45:45 -0300

    implementando filtro

--- src/App.js
+++ src/App.js
@@ -2,67 +2,10 @@ import { useEffect, useState } from "react";
 import Filters from "./Filters";
 import JobsList from "./JobsList";
 
-const filters = [
-  "Frontend",
-  "Frontend",
-  "Frontend",
-  "Frontend",
-  "Frontend",
-  "Frontend",
-  "Frontend",
-];
-
-const jobs = [
-  {
-    id: 1,
-    company: "Photosnap",
-    logo: "./images/photosnap.svg",
-    new: true,
-    featured: true,
-    position: "Senior Frontend Developer",
-    role: "Frontend",
-    level: "Senior",
-    postedAt: "1d ago",
-    contract: "Full Time",
-    location: "USA Only",
-    languages: ["HTML", "CSS", "JavaScript"],
-    tools: [],
-  },
-  {
-    id: 2,
-    company: "Manage",
-    logo: "./images/manage.svg",
-    new: true,
-    featured: true,
-    position: "Fullstack Developer",
-    role: "Fullstack",
-    level: "Midweight",
-    postedAt: "1d ago",
-    contract: "Part Time",
-    location: "Remote",
-    languages: ["Python"],
-    tools: ["React"],
-  },
-  {
-    id: 3,
-    company: "Account",
-    logo: "./images/account.svg",
-    new: true,
-    featured: false,
-    position: "Junior Frontend Developer",
-    role: "Frontend",
-    level: "Junior",
-    postedAt: "2d ago",
-    contract: "Part Time",
-    location: "USA Only",
-    languages: ["JavaScript"],
-    tools: ["React", "Sass"],
-  },
-];
-
 function App() {
   const [jobs, setJobs] = useState([]);
   const [filters, setFilters] = useState([]);
+  const [filteredJobs, setFilteredJobs] = useState([]);
 
   useEffect(function () {
     async function fetchJobs() {
@@ -73,9 +16,24 @@ function App() {
     fetchJobs();
   }, []);
 
+  useEffect(
+    function () {
+      if (filters.length === 0) {
+        setFilteredJobs(jobs);
+        return;
+      }
+      const result = jobs.filter((job) => {
+        const tags = [job.role, job.level, ...job.languages];
+        return filters.every((filter) => tags.includes(filter));
+      });
+
+      setFilteredJobs(result);
+    },
+    [filters, jobs]
+  );
+
   function handleSelectFilter(tag) {
-    if (filters.includes(tag)) return;
-    setFilters((filters) => [tag, ...filters]);
+    if (!filters.includes(tag)) setFilters((filters) => [tag, ...filters]);
   }
 
   function handleClearFilters() {
@@ -87,7 +45,7 @@ function App() {
       <div className="filters">
         <Filters filters={filters} onClearFilters={handleClearFilters} />
       </div>
-      <JobsList jobs={jobs} onSelectTag={handleSelectFilter} />
+      <JobsList jobs={filteredJobs} onSelectTag={handleSelectFilter} />
     </div>
   );
 }
